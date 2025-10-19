ac539056912ef5fb3e8dfa6e99797dc518c36ecb
ARYANPATEL-BIT <aryan.patel7291@gmial.com>
2025-10-20 00:41:05 +0530

    added ValueError exceptance

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import random
 
 guess = True
-chances = 5
+chances = 10
 tries = 0
+computer = random.randint(1,100)
 
 while guess:
 
-    user = int(input("Guess a number (1-100): "))
-    computer = random.randint(1,100)
+    try:
+        user = int(input("Guess a number (1-100): "))
+    except ValueError:
+        print("❌ Enter a number!")
+        continue
 
     if user > computer :
-
         chances -=1
         tries += 1
         print("Too High! Guess Lower")
-        print(f"{chances} left!")
-    elif user < computer:
+        print(f"{chances} chances left!")
 
+    elif user < computer:
         chances -= 1
         tries += 1
         print("Too Low! Guess Higher")
-        print(f"{chances} left!")
-    elif user == computer:
+        print(f"{chances} chances left!")
 
+    else:
+        tries += 1
         print(f"🎉Correct! You got it in {tries} tires!")
+        guess = False
+
     if chances == 0:
         guess = False
-        print("You lost! Better Luck next Time!")
+        print("You lost! Better Luck next time!")
